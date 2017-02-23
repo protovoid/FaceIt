29ed950a73f3c4e60580279c06cfb75442aa3758
Chad Williams <fadewave@gmail.com>
2017-02-23 14:31:44 -0800

    model added

--- FaceIt/FaceViewController.swift
+++ FaceIt/FaceViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class FaceViewController: UIViewController {
-
-
-
-
+  var expression = FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile) {
+    didSet {
+      updateUI()
+    }
+    
+  }
+  
+  @IBOutlet weak var faceView: FaceView! { didSet { updateUI() } }
+  
+  fileprivate var mouthCurvatures = [FacialExpression.Mouth.Frown:-1.0,.Grin:0.5,.Smile:1.0,.Smirk:-0.5,.Neutral:0.0]
+  fileprivate var eyeBrowTilts = [FacialExpression.EyeBrows.Relaxed:0.5,.Furrowed:-0.5,.Normal:0.0]
+  
+  fileprivate func updateUI() {
+    switch expression.eyes {
+    case .Open: faceView.eyesOpen = true
+    case .Closed: faceView.eyesOpen = false
+    case .Squinting: faceView.eyesOpen = false
+    }
+    faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0 // default to zero
+    faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
+    
+  }
+  
+  
 }
 
