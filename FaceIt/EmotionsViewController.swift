8c45bb716cbb6d8cc9cd8e5fc5cf300be188230a
Chad Williams <fadewave@gmail.com>
2017-02-24 13:12:04 -0800

    slit vc for ipad, nav vc with titles

---
+++ FaceIt/EmotionsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by Chad on 2/24/17.
+//  Copyright © 2017 Chad Williams. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+
+       
+
+ 
+    // MARK: - Navigation
+  
+  private let emotionalFaces: Dictionary<String,FacialExpression> = [
+    "angry" : FacialExpression(eyes: .Closed, eyeBrows: .Furrowed, mouth: .Frown),
+    "happy" : FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile),
+    "worried" : FacialExpression(eyes: .Open, eyeBrows: .Relaxed, mouth: .Smirk),
+    "mischievious" : FacialExpression(eyes: .Open, eyeBrows: .Furrowed, mouth: .Grin)
+  ]
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      var destinationvc = segue.destination
+      // if destination vc is NavController, get controller inside to segue to instead
+      if let navcon = destinationvc as? UINavigationController {
+        destinationvc = navcon.visibleViewController ?? destinationvc
+      }
+      if let facevc = destinationvc as? FaceViewController {
+        if let identifier = segue.identifier {
+          if let expression = emotionalFaces[identifier] {
+            facevc.expression = expression
+            if let sendingButton = sender as? UIButton {
+              facevc.navigationItem.title = sendingButton.currentTitle
+            }
+          }
+        }
+      }
+    
+    }
+  
+
+}
--- FaceIt/FaceViewController.swift
+++ FaceIt/FaceViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class FaceViewController: UIViewController {
+  
+  // MARK: Model
+  
   var expression = FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile) {
     didSet {
-      updateUI()
+      updateUI() // model changed, so update the View
     }
     
   }
+  
+  // MARK: View
   
   @IBOutlet weak var faceView: FaceView! {
     didSet {
@@ -36,6 +41,31 @@ class FaceViewController: UIViewController {
     }
   }
   
+  fileprivate func updateUI() {
+    if faceView != nil {
+      switch expression.eyes {
+      case .Open: faceView.eyesOpen = true
+      case .Closed: faceView.eyesOpen = false
+      case .Squinting: faceView.eyesOpen = false
+      }
+      faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0 // default to zero
+      faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
+    }
+  }
+  
+  fileprivate var mouthCurvatures = [FacialExpression.Mouth.Frown:-1.0,.Grin:0.5,.Smile:1.0,.Smirk:-0.5,.Neutral:0.0]
+  fileprivate var eyeBrowTilts = [FacialExpression.EyeBrows.Relaxed:0.5,.Furrowed:-0.5,.Normal:0.0]
+  
+    // MARK: Gesture Handlers
+  
+  func increaseHappiness() {
+    expression.mouth = expression.mouth.happierMouth()
+  }
+  
+  func decreaseHappiness() {
+    expression.mouth = expression.mouth.sadderMouth()
+  }
+  
   @IBAction func toggleEyes(_ recognizer: UITapGestureRecognizer) {
     if recognizer.state == .ended {
       switch expression.eyes {
@@ -46,27 +76,28 @@ class FaceViewController: UIViewController {
     }
   }
   
-  func increaseHappiness() {
-    expression.mouth = expression.mouth.happierMouth()
-  }
-  
-  func decreaseHappiness() {
-    expression.mouth = expression.mouth.sadderMouth()
-  }
-  
-  fileprivate var mouthCurvatures = [FacialExpression.Mouth.Frown:-1.0,.Grin:0.5,.Smile:1.0,.Smirk:-0.5,.Neutral:0.0]
-  fileprivate var eyeBrowTilts = [FacialExpression.EyeBrows.Relaxed:0.5,.Furrowed:-0.5,.Normal:0.0]
-  
-  fileprivate func updateUI() {
-    switch expression.eyes {
-    case .Open: faceView.eyesOpen = true
-    case .Closed: faceView.eyesOpen = false
-    case .Squinting: faceView.eyesOpen = false
+  func changeBrows(_ recognizer: UIRotationGestureRecognizer) {
+    switch recognizer.state {
+    case .changed,.ended:
+      if recognizer.rotation > CGFloat(M_PI/4) {
+        expression.eyeBrows = expression.eyeBrows.moreRelaxedBrow()
+        recognizer.rotation = 0.0
+      } else if recognizer.rotation < -CGFloat(M_PI/4) {
+        expression.eyeBrows = expression.eyeBrows.moreFurrowedBrow()
+        recognizer.rotation = 0.0
+      }
+    default:
+      break
     }
-    faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0 // default to zero
-    faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
-    
   }
+  
+
+  
+
+  
+
+  
+
   
   
 }
